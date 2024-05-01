746ac8276b47a798f9b84817f85cfe9646b380b1
alex <alexxpyykonen@gmail.com>
2024-05-01 23:45:09 +0300

    add the function for extracting the data before map in parsing + extract_game_data() extracts the data before game + fill_texture() fills the data for the textures path

--- srcs/extract_scene.c
+++ srcs/extract_scene.c
@@ -6,21 +6,50 @@
 /*   By: apyykone <apyykone@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/30 13:52:48 by apyykone          #+#    #+#             */
-/*   Updated: 2024/04/30 16:54:37 by apyykone         ###   ########.fr       */
+/*   Updated: 2024/05/01 23:45:06 by apyykone         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-static void	extract_data(t_scenedata *scene, char *line)
+void	fill_texture(char *texture_path, t_texture *texture)
 {
+	if (!texture->path)
+	{
+		texture->path = ft_strdup(texture_path);
+		if (!texture->path)
+			ft_clean_exit("Error: get_texture()\n");
+	}
+	else
+		texture->path = 0;
+}
+
+static void	extract_game_data(t_scenedata *scene, char **data)
+{
+	if (data[0] == 0)
+		return ;
+	else if (!ft_strcmp(data[0], "NO") && data[1] != 0 && data[2] == 0)
+		fill_texture(data[1], &scene->no_tex);
+	else if (!ft_strcmp(data[0], "SO") && data[1] != 0 && data[2] == 0)
+		fill_texture(data[1], &scene->so_tex);
+	else if (!ft_strcmp(data[0], "WE") && data[1] != 0 && data[2] == 0)
+		fill_texture(data[1], &scene->we_tex);
+	else if (!ft_strcmp(data[0], "EA") && data[1] != 0 && data[2] == 0)
+		fill_texture(data[1], &scene->ea_tex);
+	else if (data[0][0] != '\n')
+	{
+		printf("Error\nUnknown identifier: %s", data[0]);
+		exit(EXIT_SUCCESS);
+	}
 }
 
 void	extract_scene(t_scenedata *scene, char **av)
 {
-	const int	fd = open(av[1], O_RDONLY);
-	char		*line;
+	int fd;
+	char *line;
+	char **splitted_data;
 
+	fd = open(av[1], O_RDONLY);
 	if (fd < 0)
 		ft_clean_exit(CUB_OPEN_ERROR_MSG);
 	while (true)
@@ -34,7 +63,12 @@ void	extract_scene(t_scenedata *scene, char **av)
 			break ;
 		}
 		else
-			extract_data(scene, line);
+		{
+			splitted_data = ft_splits(line, "\n\t\v\f\r");
+			if (!splitted_data)
+				ft_clean_exit("Error: ft_splits()\n");
+			extract_game_data(scene, splitted_data);
+		}
 	}
 	close(fd);
 }
