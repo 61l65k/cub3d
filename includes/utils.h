89cbfc348b58bced60547bc24665eb4e7ace2535
alex <alexxpyykonen@gmail.com>
2024-05-04 12:38:51 +0300

    Errorize the get_color_rgb()

--- includes/utils.h
+++ includes/utils.h
@@ -6,7 +6,7 @@
 /*   By: apyykone <apyykone@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/26 18:00:37 by apyykone          #+#    #+#             */
-/*   Updated: 2024/05/01 23:47:24 by apyykone         ###   ########.fr       */
+/*   Updated: 2024/05/04 12:37:19 by apyykone         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,10 @@
 # define ARG_ERROR_MSG "Remember to give the cub file name ./cub3d <example.cub>\n"
 # define NOT_CUB_ERROR_MSG "The file is not a .cub file\n"
 # define CUB_OPEN_ERROR_MSG "Error: .cub open()\n"
-
+# define CUB_ERROR_COLOR \
+	"Error: get_color_rgb()\
+	- color value out of range\n"
+# define CUB_ERROR_MALLOC "Error: malloc() in ->"
 int		is_cubfile(char *file);
 void	ft_clean_exit(char *msg);
 int		is_start_of_map(char *line);
