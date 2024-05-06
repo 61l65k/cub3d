11300f6ea651f7bdd33b408743cb08317a35f05b
alex <alexxpyykonen@gmail.com>
2024-05-06 19:08:26 +0300

    Add callback for pressing x in window && cleanly exits

--- srcs/play/events.c
+++ srcs/play/events.c
@@ -6,25 +6,32 @@
 /*   By: apyykone <apyykone@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/06 18:32:15 by apyykone          #+#    #+#             */
-/*   Updated: 2024/05/06 18:40:26 by apyykone         ###   ########.fr       */
+/*   Updated: 2024/05/06 19:07:52 by apyykone         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
+static int	close_game_x(t_cubed *cubed)
+{
+	ft_clean_exit(cubed, NULL);
+	return (1);
+}
+
 static int	key_press(int keycode, t_cubed *cubed)
 {
 	printf("Key pressed: %d\n", keycode);
 	if (keycode == K_ESC)
-		ft_clean_exit(cubed, "Exiting game!\n");
-	return (0);
+		ft_clean_exit(cubed, NULL);
+	return (1);
 }
 
 void	prepare_events(t_cubed *cubed)
 {
 	const t_mlx *mlx = &cubed->mlx;
 
-	mlx_hook(mlx->win, KEY_PRESS, M_KEY_PRESS << 0, key_press, cubed);
+	mlx_hook(mlx->win, KEY_PRESS, M_KEY_PRESS, key_press, cubed);
+	mlx_hook(mlx->win, DESTROY_NOTIFY, M_DESTROY_NOTIFY, close_game_x, cubed);
 	// mlx_hook(mlx->win, 3, 1L << 1, key_release, cubed);
 	// mlx_hook(mlx->win, 17, 1L << 17, close_game, cubed);
 }
