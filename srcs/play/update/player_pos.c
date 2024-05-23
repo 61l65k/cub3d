e514080bd8a8963673b76b6e4cf6fa63bc77fcec
ttakala <ttakala@student.hive.fi>
2024-05-23 15:02:40 +0300

    allow player movement on per axis basis so we can slide across walls even if we are oriented towards them

--- srcs/play/update/player_pos.c
+++ srcs/play/update/player_pos.c
@@ -6,40 +6,54 @@
 /*   By: ttakala <ttakala@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/18 02:03:21 by apyykone          #+#    #+#             */
-/*   Updated: 2024/05/22 13:47:13 by ttakala          ###   ########.fr       */
+/*   Updated: 2024/05/23 14:57:19 by ttakala          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-static void	move(t_player *player, double angle, double move_step,
+static
+void	move(t_player *player, double angle, double move_step,
 		const t_map *map)
 {
-	const double	new_x = player->x + cos(angle) * move_step;
-	const double	new_y = player->y + sin(angle) * move_step;
-	const char		map_val = t_map_get(map, (int)new_x, (int)new_y);
+	double	new_x;
+	double	new_y;
+	char	map_char;
 
-	if (map_val != '\0' && map_val != '1')
+	new_x = player->x + cos(angle) * move_step;
+	new_y = player->y + sin(angle) * move_step;
+	new_x = fmin(fmax(new_x, 0.01), map->width - 0.01);
+	new_y = fmin(fmax(new_y, 0.01), map->height - 0.01);
+	map_char = t_map_get(map, new_x, new_y);
+	if (map_char != '1')
 	{
 		player->x = new_x;
-		if (player->x < 0)
-			player->x = 0;
 		player->y = new_y;
-		if (player->y < 0)
-			player->y = 0;
+		return ;
 	}
+	map_char = t_map_get(map, player->x, new_y);
+	if (map_char != '1')
+	{
+		player->y = new_y;
+		return ;
+	}
+	map_char = t_map_get(map, new_x, player->y);
+	if (map_char != '1')
+		player->x = new_x;
 }
 
 void	update_player_position(t_player *player, const t_map *map)
 {
 	if (player->z_move)
 	{
-		move(player, player->rotation_angle, (double)player->z_move
+		move(player, player->rotation_angle,
+			(double)player->z_move
 			* player->move_speed, map);
 	}
 	if (player->x_move)
 	{
-		move(player, player->rotation_angle + deg2rad(90), (double)player->x_move
+		move(player, player->rotation_angle + deg2rad(90),
+			(double)player->x_move
 			* player->move_speed, map);
 	}
 }
