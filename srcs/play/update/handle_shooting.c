09ee63ec01311bc6be0dad816dfb187e24ba8924
alex <alexxpyykonen@gmail.com>
2024-05-23 16:14:16 +0300

    + wrench will now build walls to a natural place from the player + raygun destroys 4 adjacent walls from the map can be increased to any amount

--- srcs/play/update/handle_shooting.c
+++ srcs/play/update/handle_shooting.c
@@ -6,7 +6,7 @@
 /*   By: apyykone <apyykone@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/22 09:19:20 by apyykone          #+#    #+#             */
-/*   Updated: 2024/05/23 15:21:05 by apyykone         ###   ########.fr       */
+/*   Updated: 2024/05/23 16:13:29 by apyykone         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,12 +27,37 @@ static void	handle_akimbos_shooting(t_cubed *cubed)
 
 static void	handle_wrench_shooting(t_cubed *cubed)
 {
-	const int	map_x = (int)(cubed->player.x);
-	const int	map_y = (int)(cubed->player.y + 1);
+	const float	distance = 3.0;
+	const float	dir_x = cos(cubed->player.rotation_angle) * distance;
+	const float	dir_y = sin(cubed->player.rotation_angle) * distance;
+	const int	map_x = (int)(cubed->player.x + dir_x);
+	const int	map_y = (int)(cubed->player.y + dir_y);
 
 	if (t_map_get(&cubed->scene.map, map_x, map_y) == '0')
 	{
 		t_map_insert(&cubed->scene.map, map_x, map_y, '1');
+	}
+}
+
+static void	destroy_adjacent_walls(t_cubed *cubed, int map_x, int map_y,
+		int depth)
+{
+	int	dx;
+	int	dy;
+
+	if (depth == 4 || t_map_get(&cubed->scene.map, map_x, map_y) != '1')
+		return ;
+	t_map_insert(&cubed->scene.map, map_x, map_y, '0');
+	depth = depth + 1;
+	dx = -2;
+	while (++dx <= 1)
+	{
+		dy = -2;
+		while (++dy <= 1)
+		{
+			if (dx != 0 || dy != 0)
+				destroy_adjacent_walls(cubed, map_x + dx, map_y + dy, depth);
+		}
 	}
 }
 
@@ -42,30 +67,9 @@ static void	handle_raygun_shooting(t_cubed *cubed)
 	const t_ray	*middle_ray = &cubed->rays.ray_array[middle_ray_index];
 	const int	map_x = (int)middle_ray->x;
 	const int	map_y = (int)middle_ray->y;
-	int			dx;
-	int			dy;
-	int			adj_map_x;
-	int			adj_map_y;
 
-	dx = -1;
-	while (dx <= 1)
-	{
-		dy = -1;
-		while (dy <= 1)
-		{
-			if (dx || dy)
-			{
-				adj_map_x = map_x + dx;
-				adj_map_y = map_y + dy;
-				if (t_map_get(&cubed->scene.map, adj_map_x, adj_map_y) == '1')
-				{
-					t_map_insert(&cubed->scene.map, adj_map_x, adj_map_y, '0');
-				}
-			}
-			dy++;
-		}
-		dx++;
-	}
+	if (t_map_get(&cubed->scene.map, map_x, map_y) == '1')
+		destroy_adjacent_walls(cubed, map_x, map_y, 0);
 }
 
 void	handle_shooting(t_cubed *cubed)
@@ -86,8 +90,6 @@ void	handle_shooting(t_cubed *cubed)
 	}
 	else if (current_weapon->type == WEAPON_RAYGUN)
 	{
-		handle_akimbos_shooting(cubed);
-	}
-	else
 		handle_raygun_shooting(cubed);
+	}
 }
